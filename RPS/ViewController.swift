86e8299ef2e46825be498a1764b154a2cec7b849
Vadim Colcev <vadimkolchev@gmail.com>
2020-11-28 22:01:22 +0200

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Vadim Colcev on 11/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
